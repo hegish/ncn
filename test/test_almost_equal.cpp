e3d1e28848c1de838b5028dc1a7187a40df3b76f
Jan Hegewald <jan.hegewald@awi.de>
2019-01-17 15:08:07 +0100

    - rename initial test function - introduce more tests for almost_equal

--- test/test_almost_equal.cpp
+++ test/test_almost_equal.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 namespace {
 
-   TEST(ncn, almostequalxx)
+   TEST(ncn_almostequal, simpledoubles)
    {
       double d1 = 0.2;
       double d2 = 1 / sqrt(5) / sqrt(5);
@@ -21,4 +21,26 @@ namespace {
       cout<<"L min "<<numeric_limits<long double>::min()<<" L max "<<numeric_limits<long double>::max()<<endl;
    }
 
+   
+   TEST(ncn_almostequal, big_numeric_limits)
+   {
+      EXPECT_TRUE(almost_equal(numeric_limits<float>::max(), numeric_limits<float>::max(), 2));
+      EXPECT_FALSE(almost_equal(numeric_limits<float>::max()*0.51, numeric_limits<float>::max()*0.54, 2));
+   }
+   
+   
+   TEST(ncn_almostequal, bignums)
+   {
+      EXPECT_TRUE(almost_equal(2.0e38F, 2.0e38F, 2));
+      EXPECT_FALSE(almost_equal(2.1e38F, 2.4e38F, 2));
+      EXPECT_TRUE(almost_equal(2.000000001e38F, 2.000000004e38F, 2));
+      EXPECT_FALSE(almost_equal(2.000001e37F, 2.000004e37F, 2));
+   }
+
+
+   TEST(ncn_almostequal, bigandsmallnums)
+   {
+      EXPECT_FALSE(almost_equal(2.0e30F, 2.0e-30F, 2));
+   }
+   
 }  // namespace
