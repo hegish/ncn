dc4c26a11643b63158d8cc76dbe4e3d95b1dd5e0
Jan Hegewald <jan.hegewald@awi.de>
2019-01-18 12:23:33 +0100

    add include guard to almost_equal header

--- lib/almost_equal.h
+++ lib/almost_equal.h
@@ -1,3 +1,6 @@
-
+#ifndef almost_equal_6FB10D8D_A53B_4C9F_B2DF_C0DD029808EA
+#define almost_equal_6FB10D8D_A53B_4C9F_B2DF_C0DD029808EA
 
 template<typename T> bool almost_equal(T x, T y, int ulp);
+
+#endif
