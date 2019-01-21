da43f692388355381a2d8f69565a955915df1a16
Jan Hegewald <jan.hegewald@awi.de>
2019-01-21 14:30:12 +0100

    remove console output from almost equal unit tests

--- test/test_almost_equal.cpp
+++ test/test_almost_equal.cpp
@@ -15,10 +15,6 @@ namespace {
 
       EXPECT_DOUBLE_EQ(d1, d2);
       EXPECT_TRUE(almost_equal(d1, d2, 2));
-
-      cout<<"F min "<<numeric_limits<float>::min()<<" F max "<<numeric_limits<float>::max()<<endl;
-      cout<<"min "<<numeric_limits<double>::min()<<" max "<<numeric_limits<double>::max()<<endl;
-      cout<<"L min "<<numeric_limits<long double>::min()<<" L max "<<numeric_limits<long double>::max()<<endl;
    }
 
    
