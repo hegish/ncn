5475f36296ac22df5f9915cfa26872c82d6c883b
Jan Hegewald <jan.hegewald@awi.de>
2019-01-21 14:11:00 +0100

    make the units in the last place argument an unsigned int for comparing floating point values

--- lib/almost_equal.cpp
+++ lib/almost_equal.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-template<typename T> bool almost_equal(T x, T y, int ulp)
+template<typename T> bool almost_equal(T x, T y, unsigned int ulp)
 {
    // the IEEE standard says that any comparison operation involving a NAN must return false
    if( isnan(x) || isnan(y) )
@@ -17,6 +17,6 @@ template<typename T> bool almost_equal(T x, T y, int ulp)
    return abs(x-y) <= abs(numeric_limits<T>::epsilon()*x+numeric_limits<T>::epsilon()*y) * ulp;
 }
 // explicitly create template definitions
-template bool almost_equal(float x, float y, int ulp);
-template bool almost_equal(double x, double y, int ulp);
-template bool almost_equal(long double x, long double y, int ulp);
+template bool almost_equal(float x, float y, unsigned int ulp);
+template bool almost_equal(double x, double y, unsigned int ulp);
+template bool almost_equal(long double x, long double y, unsigned int ulp);
