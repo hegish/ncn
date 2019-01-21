06a1edd1441a30731f085515b3e3f2650d7f0a4b
Jan Hegewald <jan.hegewald@awi.de>
2019-01-21 13:40:22 +0100

    be able to set path to a global fixture dir via CLI arguments

---
+++ test/globals.h
@@ -0,0 +1,8 @@
+#ifndef globals_D5F0FA3E_A2CB_4047_A72E_A745BA3BAA92
+#define globals_D5F0FA3E_A2CB_4047_A72E_A745BA3BAA92
+
+#include <string>
+
+extern std::string fixture_dir;
+
+#endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,16 @@
 #include "gtest/gtest.h"
+#include "globals.h"
 
+std::string fixture_dir; // declare global variable at file scope
 
 int main(int argc, char **argv)
 {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+   ::testing::InitGoogleTest(&argc, argv);
+   
+   // set fixture dir from argv
+   std::vector<std::string> args(argv+1, argv+argc);
+   assert(args.size() > 0);
+   fixture_dir = args[0];
+   
+   return RUN_ALL_TESTS();
 }
