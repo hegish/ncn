e8220a837a3950d42bd1b50d6927821939da26f1
Jan Hegewald <jan.hegewald@awi.de>
2019-09-25 18:00:01 +0200

    change missing argument error for unit tests to throw even if assertions would be omitted

--- test/main.cpp
+++ test/main.cpp
@@ -5,11 +5,13 @@ std::string fixture_dir; // declare global variable at file scope
 
 int main(int argc, char **argv)
 {
+   if(argc < 2)
+      throw std::runtime_error("missing path to fixtures dir in args");
+
    ::testing::InitGoogleTest(&argc, argv);
    
    // set fixture dir from argv
    std::vector<std::string> args(argv+1, argv+argc);
-   assert(args.size() > 0 && "missing path to fixtures dir");
    fixture_dir = args[0];
    
    return RUN_ALL_TESTS();
